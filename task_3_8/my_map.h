255f5aae82dd51db242171a9a16c00571fe14b9b
Klishin Ilya <klishin-ilya@mail.ru>
2020-06-24 13:40:55 +0300

    task_3_8 update

--- task_3_8/my_map.h
+++ task_3_8/my_map.h
@@ -20,42 +20,35 @@ class MyMap {
 public:
 	//вставка
 	TVa& insert(const TKy& k, const TVa& val) {
-		//если хранилище пустое, просто вставим
-		if (m_nodes.size() == 0) {
+		int i = get_node_index(k);
+		//если нужно добавить в конец
+		if (i == m_nodes.size()) {
 			m_nodes.push_back({ k, val });
-			return m_nodes[0].value;
-		} else {
-			
-			auto node_pos = calc_node_pos(k);
-			//если нужно добавить в конец
-			if (node_pos == m_nodes.size()) {
-				m_nodes.insert(m_nodes.cbegin() + node_pos, { k, val });
-			}
-			else {
-				if (m_nodes[node_pos].key == k)
-					throw std::invalid_argument("Map support only unique keys");
-
-				m_nodes.insert(m_nodes.cbegin() + node_pos, { k, val });
-			}
-			return m_nodes[node_pos].value;
 		}
+		else {
+			if (m_nodes[i].key == k)
+				throw std::invalid_argument("Map support only unique keys");
+
+			m_nodes.insert(m_nodes.cbegin() + i, { k, val });
+		}
+		return m_nodes[i].value;
 	}
 
 	//удаление записи
 	void remove(const TKy& k) {
-		auto node_pos = get_node_pos(k);
-		if (node_pos < 0)
+		int i = get_node_index(k);
+		if (i >= m_nodes.size() || m_nodes[i].key != k)
 			throw std::invalid_argument("Key not exists in map");
-		m_nodes.erase(m_nodes.cbegin() + node_pos);
+		m_nodes.erase(m_nodes.cbegin() + i);
 	}
 
 	//поиск записи
 	bool try_get(const TKy& k, TVa& outVal) {
-		auto node_pos = get_node_pos(k);
-		if (node_pos < 0)
+		int i = get_node_index(k);
+		if (i >= m_nodes.size() || m_nodes[i].key != k)
 			return false;
 		else
-			outVal = m_nodes[node_pos].value;
+			outVal = m_nodes[i].value;
 		return true;
 	}
 
@@ -69,52 +62,23 @@ public:
 private:
 	//внутренности ассоциативного массива
 	
-	//ищем место для вставки
-	int calc_node_pos(const TKy& k) const {
-		//если массив пуст, или заданный ключ меньше самого меньшего имеющегося
-		if (m_nodes.size() == 0 || (k < m_nodes[0].key))
-			return 0;
-		else if (m_nodes[m_nodes.size() - 1].key < k) //если заданный ключ больше всех имеющихся
-			return m_nodes.size();
-
-		int i = 0; // текущая позиция в массиве
-		int s = m_nodes.size(); //сохраним размер, что бы не дергать функцию
-		int m = 0; // середина массива
-		
-		//пока не переберем все элементы
-		while (i < s - 1) {
-			m = (i + s) / 2; //считаем середину
-			//
-			if (k < m_nodes[m].key)
-				s = m;
-			else if (k == m_nodes[m].key)
-				i = m + 1;
-		}
-		return i;
-	}
-
 	//поиск индекса ключа
-	int get_node_pos(const TKy& k) {
-		if (m_nodes.size() == 0 || (k < m_nodes[0].key))
-			return -1;
-		else if (m_nodes[m_nodes.size() - 1].key < k)
-			return -1;
+	int get_node_index(const TKy& k) {
+		
+		if (m_nodes.size() == 0)
+			return 0;
 
-		int i = 0;
-		int s = m_nodes.size();
-		int m = 0;
-
-		while (i < s - 1) {
-			m = (i + s) / 2;
-			if (k < m_nodes[m].key)
-				s = m;
-			else {
-				if (k == m_nodes[m].key)
-					return m;
-				i = m + 1;
-			}
+		int l = -1; //левая граница
+		int r = m_nodes.size(); //правая
+		
+		while (l < r - 1) {
+			int m = (l + r) / 2;
+			if (m_nodes[m].key < k)
+				l = m;
+			else
+				r = m;
 		}
-		return i;
+		return r;
 	}
 
 	//запись в массиве
